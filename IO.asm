c4173dff80b358cd14d717d2de2bb8e121a0a728
Wilder Santos Carvalho <wcarvalho98@gmail.com>
2018-02-19 16:50:10 -0300

    Pintando o player...

--- IO.asm
+++ IO.asm
@@ -1,19 +1,18 @@
-# Como iniciar: 	0- Open MARS
-#			1- Tools -> Bitmap Display
-#			2- unit width in pixel: 8 / unit height in pixel: 8
-#			3- Display Width in Pixels: 512 / Display Height in Pixels: 256
-#			4- Tools -> Keyboard and Display
-#			5- Connect to MIPS
-#			6- Assemble and RUN
+# Como iniciar: 0- Open MARS
+#		1- Tools -> Bitmap Display
+#		2- unit width in pixel: 8 / unit height in pixel: 8
+#		3- Display Width in Pixels: 512 / Display Height in Pixels: 256
+#		4- Tools -> Keyboard and Display
+#		5- Connect to MIPS
+#		6- Assemble and RUN
 
 .kdata
 
-cor:		.word 0x000000FF
+cor:		.word 0x0000CCFF
 bitmap_address:	.word 0x10010000
 bitmap_size:	.word 2048		# (512 x 256)/8 pixels
-jogador_cor:	.word 0x00000000
-jogador_pos:	.word 0x10011A7C
-jogador_size:	.word 1			# 8 x 8 pixels
+jogador_cor:	.word 0x00FF0000
+jogador_pos:	.word 0x10011B7C
 
 .text
 .globl main
@@ -38,6 +37,16 @@ backgroud_loop:
 player:
 	lw	$t0, bitmap_address
 	lw	$t1, jogador_cor
-	
+	lw	$t2, jogador_pos
+	sw	$t1, ($t2)
+	sw	$t1, 7288($t0)
+	sw	$t1, 7292($t0)
+	sw	$t1, 7296($t0)
+	sw	$t1, 7540($t0)
+	sw	$t1, 7544($t0)
+	sw	$t1, 7548($t0)
+	sw	$t1, 7552($t0)
+	sw	$t1, 7556($t0)
+	jr	$ra
 	
 exit:
