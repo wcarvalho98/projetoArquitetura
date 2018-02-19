d34ca84b6e6a7ca634d32166c43d1d859015b751
Wilder Santos Carvalho <wcarvalho98@gmail.com>
2018-02-19 18:22:23 -0300

    Movendo pra direita e esquerda

--- IO.asm
+++ IO.asm
@@ -12,9 +12,8 @@
 cor:		.word 0x0000CCFF
 bitmap_address:	.word 0x10010000
 bitmap_size:	.word 2048		# (512 x 256)/8 pixels
-jogador_cor:	.word 0x00000000
-jogador_pos:	.word 0x10011A7C
-jogador_size:	.word 1			# 8 x 8 pixels
+jogador_cor:	.word 0x00FF0000
+jogador:	.space 16		# 4x4 blocos
 
 .text
 .globl main
@@ -40,11 +39,52 @@ backgroud_loop:
 	jr	$ra
 	
 #################################################################################
-#	Pintando o player e povoando o vetor jogador com o seu lugar na memï¿½ria	#
+#	Pintando o player e povoando o vetor jogador com o seu lugar na memória	#
 #################################################################################
 player:
 	lw	$t0, bitmap_address
 	lw	$t1, jogador_cor
+	li	$t2, 0x10011B7C
+	sw	$t2, jogador
+	li	$t2, 0x10011C78
+	sw	$t2, jogador + 4 
+	li	$t2, 0x10011C7C
+	sw	$t2, jogador + 8
+	li	$t2, 0x10011C80
+	sw	$t2, jogador + 12
+	move	$t2, $zero
+player_p:
+	lw	$t3, jogador($t2)
+	sw	$t1, ($t3)
+	addi	$t2, $t2, 4
+	blt	$t2, 16, player_p
+	jr	$ra
+	
+#################################################################################
+#	Movendo o jogador para a direita					#
+#################################################################################
+mover_direita:
+	move	$t2, $zero
+direita_loop:
+	lw	$t3, jogador($t2)
+	addi	$t3, $t3, 4
+	sw	$t3, jogador($t2)
+	addi	$t2, $t2, 4
+	blt	$t2, 16, direita_loop
+	jr	$ra
+	
+#################################################################################
+#	Movendo o jogador para a esquerda					#
+#################################################################################
+mover_esquerda:
+	move	$t2, $zero
+esquerda_loop:
+	lw	$t3, jogador($t2)
+	addi	$t3, $t3, -4
+	sw	$t3, jogador($t2)
+	addi	$t2, $t2, 4
+	blt	$t2, 16, esquerda_loop
+	jr	$ra
 	
 #################################################################################
 #	Retorna $v0 com o valor 0x01, 0x02 ou 0x03, indicando			#
