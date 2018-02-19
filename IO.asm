f580c96af84facfedec9faf0a503e1563709849a
Wilder Santos Carvalho <wcarvalho98@gmail.com>
2018-02-19 18:07:48 -0300

    Começo de captação.

--- IO.asm
+++ IO.asm
@@ -1,13 +1,14 @@
-# Como iniciar: 0- Open MARS
-#		1- Tools -> Bitmap Display
-#		2- unit width in pixel: 8 / unit height in pixel: 8
-#		3- Display Width in Pixels: 512 / Display Height in Pixels: 256
-#		4- Tools -> Keyboard and Display
-#		5- Connect to MIPS
-#		6- Assemble and RUN
+#################################################################################
+# Como iniciar: 0- Open MARS							#
+#		1- Tools -> Bitmap Display					#
+#		2- unit width in pixel: 8 / unit height in pixel: 8		#
+#		3- Display Width in Pixels: 512 / Display Height in Pixels: 256	#
+#		4- Tools -> Keyboard and Display				#
+#		5- Connect to MIPS						#
+#		6- Assemble and RUN						#
+#################################################################################
 
 .kdata
-
 cor:		.word 0x0000CCFF
 bitmap_address:	.word 0x10010000
 bitmap_size:	.word 2048		# (512 x 256)/8 pixels
@@ -21,6 +22,9 @@ main:
 	jal	player
 	j 	exit
 	
+#################################################################################
+#	Pintando o background							#
+#################################################################################
 background:
 	lw	$t0, bitmap_address
 	lw	$t1, cor
@@ -34,6 +38,9 @@ backgroud_loop:
 	blt	$t3, $t2, backgroud_loop
 	jr	$ra
 	
+#################################################################################
+#	Pintando o player e povoando o vetor jogador com o seu lugar na memória	#
+#################################################################################
 player:
 	lw	$t0, bitmap_address
 	lw	$t1, jogador_cor
@@ -53,4 +60,27 @@ player_p:
 	blt	$t2, 16, player_p
 	jr	$ra
 	
+#################################################################################
+#	Retorna $v0 com o valor 0x01, 0x02 ou 0x03, indicando			#
+#	direita, esquerda e disparo respectivamente.				#
+#################################################################################
+obter_tecla:
+	lw $t0 0xFFFF0004		
+obter_tecla_direita:
+	bne $t0 100 obter_tecla_esquerda
+	li $v0 0x01000000
+	j obter_tecla_voltar
+obter_tecla_esquerda:
+	bne $t0 97 obter_tecla_disparo
+	li $v0 0x02000000
+	j obter_tecla_voltar
+obter_tecla_disparo:
+	bne $t0 32 obter_tecla_voltar
+	li $v0 0x03000000		
+obter_tecla_voltar:
+	jr $ra
+	
+#################################################################################
+#	Saindo do jogo :c							#
+#################################################################################
 exit:
